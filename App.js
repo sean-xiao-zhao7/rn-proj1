dbd1dfc71beac4ccfbfb7735152cfab2421be11f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-06-08 12:45:38 -0400

    Add goals

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [text, setText] = useState("");
+    const [goals, setGoals] = useState([]);
+    const inputChangeHandler = (text) => {
+        setText(text);
+    };
+    const addGoalHandler = (e) => {
+        setGoals((prevGoals) => {
+            return [...prevGoals, text];
+        });
+        setText("");
+    };
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                placeholder="Enter a goal"
+                onChangeText={inputChangeHandler}
+                value={text}
+            />
+            <Button onPress={addGoalHandler} title="Add" />
+            <View>
+                <Text>List of goals</Text>
+                {goals.map((goal) => {
+                    return <Text key={goal + Math.random()}>{goal}</Text>;
+                })}
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        padding: 50,
+    },
 });
